f3a61c5f44f28668db16a5eb18c463846c1c1468
Elena <lenajuj@gmail.com>
2018-06-04 22:30:14 +0300

    create new array

--- index.php
+++ index.php
@@ -5,18 +5,24 @@ $temp = "<pre>";
 $odd = "";
 $even = "";
 
-foreach ($arr as $value) {
-    if($value % 2 == 0){
+foreach ($arr as $key => $value) {
+    if($value % 2 == 0) {
         $even .= ($value * $value) . " ";
+        $arr[$key] = $value * $value;
     }
-    if($value % 2 != 0){
+
+    if($value % 2 != 0) {
         $odd .= ($value / 2) . " ";
+        $arr[$key] = $value / 2;
     }
 }
 
 $temp .= "Четные: $even";
 $temp .= "\n";
 $temp .= "Нечетные: $odd";
+
 $temp .= "</pre>";
 
 echo $temp;
+
+var_dump($arr);
